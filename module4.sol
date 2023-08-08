cc022e62691eda2720966b1e37af574f905f2694
kashishguliani <126547582+kashishguliani@users.noreply.github.com>
2023-08-09 00:59:01 +0530

    Update module4.sol

--- module4.sol
+++ module4.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable{
-     constructo() ERC20("Degen Token", "DEGEN") {
+     constructor() ERC20("Degen Token", "DEGEN") {
        _mint(msg.sender, 1000000 * 10 ** decimals());
  }
     function mint(address to, uint256 amount) public onlyOwner {
