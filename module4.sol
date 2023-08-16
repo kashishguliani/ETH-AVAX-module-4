4ee2a996167812be9065a9c2aaf1a6e48ac353fd
kashishguliani <126547582+kashishguliani@users.noreply.github.com>
2023-08-16 23:45:26 +0530

    Update module4.sol

--- module4.sol
+++ module4.sol
@@ -1,18 +1,52 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract DegenToken {
+    string public name = "Kashish";
+    string public symbol = "kash";
+    uint8 public decimals = 18; // 18 decimal places, similar to most ERC-20 tokens
+    uint256 public totalSupply;
 
-contract DegenToken is ERC20, Ownable{
-     constructor() ERC20("Degen Token", "DEGEN") {
-       _mint(msg.sender, 1000000 * 10 ** decimals());
- }
-    function mint(address to, uint256 amount) public onlyOwner {
-    _mint(to, amount);
- }
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
- }
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 initialSupply) {
+        totalSupply = initialSupply * 10**uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 value) external returns (bool) {
+        require(to != address(0), "Invalid address");
+        require(value <= balanceOf[msg.sender], "Insufficient balance");
+
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) external returns (bool) {
+        allowance[msg.sender][spender] = value;
+
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool) {
+        require(from != address(0), "Invalid address");
+        require(to != address(0), "Invalid address");
+        require(value <= balanceOf[from], "Insufficient balance");
+        require(value <= allowance[from][msg.sender], "Allowance exceeded");
+
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+
+        emit Transfer(from, to, value);
+        return true;
+    }
 }
